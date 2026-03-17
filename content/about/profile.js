71c9dea14db4b5a0eafae9725902ccc37a65ad40
Bob Tianqi Wei <weitianqi3598@hotmail.com>
2026-03-17 14:59:06 -0700

    Move about hero links and portrait data into profile

--- content/about/profile.js
+++ content/about/profile.js
@@ -2,5 +2,34 @@
 window.aboutProfile = {
   nameHtml: "Bob Tianqi Wei<br>魏 天祺",
   pronounsHtml: "\"Tien-Chi\", he/they <a href=\"http://www.mypronouns.org/he-him\" target=\"_blank\" class=\"link-in-paragraph-grey\">(What is this?)</a>",
-  bioHtml: "I am an HCI researcher at the <a href=\"https://bid.berkeley.edu/\" target=\"_blank\" class=\"link-in-paragraph\">Berkeley Institute of Design Lab (BiD)</a> in EECS, working with Professor <a href=\"https://people.eecs.berkeley.edu/~bjoern/\" target=\"_blank\" class=\"link-in-paragraph\">Björn Hartmann</a> and <a href=\"https://shmuh.co/\" target=\"_blank\" class=\"link-in-paragraph\">Shm Almeda</a>. I hold an MDes in Human–Computer Interaction from the UC Berkeley College of Engineering and a BA in Industrial Design from Tsinghua University. <br>‍<br>My research sits at the intersection of human–computer interaction, AI, and embodied / material interfaces. I design and study systems that use AI and novel hardware to support tacit skill learning, creative work, and responsible forms of automation.<br>‍<br>Outside of research, I am a classical musician, designer and frontend engineer, and I often bring these practices into my teaching and interactive prototypes.<br>"
+  bioHtml: "I am an HCI researcher at the <a href=\"https://bid.berkeley.edu/\" target=\"_blank\" class=\"link-in-paragraph\">Berkeley Institute of Design Lab (BiD)</a> in EECS, working with Professor <a href=\"https://people.eecs.berkeley.edu/~bjoern/\" target=\"_blank\" class=\"link-in-paragraph\">Björn Hartmann</a> and <a href=\"https://shmuh.co/\" target=\"_blank\" class=\"link-in-paragraph\">Shm Almeda</a>. I hold an MDes in Human–Computer Interaction from the UC Berkeley College of Engineering and a BA in Industrial Design from Tsinghua University. <br>‍<br>My research sits at the intersection of human–computer interaction, AI, and embodied / material interfaces. I design and study systems that use AI and novel hardware to support tacit skill learning, creative work, and responsible forms of automation.<br>‍<br>Outside of research, I am a classical musician, designer and frontend engineer, and I often bring these practices into my teaching and interactive prototypes.<br>",
+  links: [
+    {
+      href: "/website_files/bobtianqiwei_cv.pdf",
+      label: "Full CV"
+    },
+    {
+      href: "https://github.com/bobtianqiwei",
+      label: "GitHub"
+    },
+    {
+      href: "https://www.linkedin.com/in/bobtianqiwei/",
+      label: "LinkedIn"
+    },
+    {
+      href: "https://www.youtube.com/channel/UCfxLPK0-77H4iC3pHukSrIg",
+      label: "YouTube"
+    }
+  ],
+  portrait: {
+    front: {
+      src: "/images/bobgrand-canyon.jpg",
+      sizes: "(max-width: 479px) 96vw, (max-width: 767px) 97vw, (max-width: 991px) 23vw, 25vw",
+      srcset: "/images/7efd805030b977885f19932a64fcac7b_bobgrand-canyon-p-500.jpg 500w, /images/7efd805030b977885f19932a64fcac7b_bobgrand-canyon-p-800.jpg 800w, /images/7efd805030b977885f19932a64fcac7b_bobgrand-canyon-p-1080.jpg 1080w, /images/7efd805030b977885f19932a64fcac7b_bobgrand-canyon-p-1600.jpg 1600w, /images/7efd805030b977885f19932a64fcac7b_bobgrand-canyon-p-2000.jpg 2000w, /images/7efd805030b977885f19932a64fcac7b_bobgrand-canyon-p-2600.jpg 2600w, /images/7efd805030b977885f19932a64fcac7b_bobgrand-canyon-p-3200.jpg 3200w, /images/bobgrand-canyon.jpg 3228w"
+    },
+    back: {
+      src: "/images/bobgrand-canyon-square-smile.jpg"
+    },
+    caption: "Photo by Yingjia Luo from 10/18/2025"
+  }
 };
