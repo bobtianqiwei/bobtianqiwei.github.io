e974f58b813d57f29076c6ee24f820ffbd116adc
Bob Tianqi Wei <weitianqi3598@hotmail.com>
2026-03-16 17:19:48 -0700

    Extract about page content into shared data scripts

---
+++ content/about/collaborations.js
@@ -0,0 +1,55 @@
+// collaborations.js developed by Bob Tianqi Wei
+window.aboutCollaborations = [
+  {
+    href: "https://people.eecs.berkeley.edu/~bjoern/",
+    nameHtml: "<strong>Bjoern Hartmann</strong>"
+  },
+  {
+    href: "https://www.caseysimone.com/",
+    nameHtml: "Caseysimone Ballestas"
+  },
+  {
+    href: "https://design.berkeley.edu/profiles/chris-myers",
+    nameHtml: "Chris Myers"
+  },
+  {
+    href: "https://www.createanything.com/",
+    image: {
+      src: "/images/Anything-logo.svg",
+      className: "image-in-collaborations createanything-logo"
+    }
+  },
+  {
+    href: "https://bse.berkeley.edu/dor-abrahamson",
+    nameHtml: "<strong>Dor Abrahamson</strong>"
+  },
+  {
+    href: "https://www.ischool.berkeley.edu/people/kimiko-ryokai",
+    nameHtml: "Kimiko Ryokai"
+  },
+  {
+    href: "https://me.berkeley.edu/people/lining-yao/",
+    nameHtml: "<strong>Lining Yao</strong>"
+  },
+  {
+    href: "https://www.ad.tsinghua.edu.cn/info/1226/15186.htm",
+    nameHtml: "<strong>Lintao Tang</strong>"
+  },
+  {
+    href: "https://www.instagram.com/sabbatical.lab",
+    customClass: "sabbatical",
+    nameHtml: "sabbatical."
+  },
+  {
+    href: "https://shmuh.co/",
+    nameHtml: "Shm Garanganao Almeda"
+  },
+  {
+    href: "https://www.ischool.berkeley.edu/people/stefanie-hutka",
+    nameHtml: "Stefanie Hutka"
+  },
+  {
+    href: "https://yutianyu.design/",
+    nameHtml: "Tianyu Yu"
+  }
+];
---
+++ content/about/friends.js
@@ -0,0 +1,113 @@
+// friends.js developed by Bob Tianqi Wei
+window.aboutFriends = [
+  {
+    href: "https://abigailchen.me",
+    name: "Abigail Chen",
+    descriptionHtml: "Lychee Sushi Mother <br>Best Carillon Partner Ever"
+  },
+  {
+    href: "https://www.andyshensiyang.com/",
+    name: "Andy Siyang Shen",
+    descriptionHtml: "Interdisciplinary Designer"
+  },
+  {
+    href: "https://www.baeuke.com/",
+    name: "Baurzhan Abenov",
+    descriptionHtml: "Design Engineer<br>Computer Scientist"
+  },
+  {
+    href: "https://space.bilibili.com/475419922",
+    name: "Bingjing Zhang",
+    descriptionHtml: "Integrated Circuit Designer<br>Musician"
+  },
+  {
+    href: "https://changliu-drpatient.github.io/",
+    name: "Chang Liu",
+    descriptionHtml: "ML Researcher<br>Computer Scientist<br>"
+  },
+  {
+    href: "https://cmarcelino.com/",
+    name: "Christine Marcelino",
+    descriptionHtml: "Industrial Designer<br>Creative Technologist<br>"
+  },
+  {
+    href: "https://guang-yng.github.io/",
+    name: "Guang Yang",
+    descriptionHtml: "AI for Music Researcher<br>Computer Scientist<br>"
+  },
+  {
+    href: "https://hilamor.com/",
+    name: "Hila Mor",
+    descriptionHtml: "HCI Researcher<br>Designer, Artist<br>"
+  },
+  {
+    href: "https://isabel.li",
+    name: "Isabel Li",
+    descriptionHtml: "Artist <br>Computer Scientist"
+  },
+  {
+    href: "https://www.linkedin.com/in/jiashu-liu-b129a7190",
+    name: "Jiashu Liu",
+    descriptionHtml: "Computer Scientist"
+  },
+  {
+    href: "https://jiayi.ink",
+    name: "Jiayi Liu",
+    descriptionHtml: "Designer<br>Artist"
+  },
+  {
+    href: "https://katherine-liu.com/",
+    name: "Katherine Liu",
+    descriptionHtml: "Creative Technologist <br>UX Designer"
+  },
+  {
+    href: "https://scholar.google.com/citations?user=_Y93NacAAAAJ&amp;hl=en&amp;oi=ao",
+    name: "Kondo Gen",
+    descriptionHtml: "Civil Engineer<br>"
+  },
+  {
+    href: "https://lilucy.at/",
+    name: "Lucy Li",
+    descriptionHtml: "HCI Researcher<br>Designer"
+  },
+  {
+    href: "https://www.quincykuang.com/",
+    name: "Quincy Kuang",
+    descriptionHtml: "HCI Researcher<br>Industrial Designer"
+  },
+  {
+    href: "https://www.rachelqzhang.com",
+    name: "Rachel Qingzhu Zhang",
+    descriptionHtml: "Phantastic Designer"
+  },
+  {
+    href: "https://www.sshen.info/",
+    name: "Shayne Shen",
+    descriptionHtml: "Graphic &amp; Visual Designer"
+  },
+  {
+    href: "https://shmuh.co/",
+    name: "Shm Garanganao Almeda",
+    descriptionHtml: "Artist <br>Computer Scientist"
+  },
+  {
+    href: "https://www.stephaniewang.design/",
+    name: "Stephanie Wang",
+    descriptionHtml: "UX Designer<br>"
+  },
+  {
+    href: "https://yutianyu.design/",
+    name: "Tianyu Yu",
+    descriptionHtml: "HCI&nbsp;Researcher"
+  },
+  {
+    href: "https://sites.google.com/view/yaningli/about",
+    name: "Yaning Li",
+    descriptionHtml: "HCI Researcher"
+  },
+  {
+    href: "https://yuchenguommm.github.io/",
+    name: "Yuchen Guo",
+    descriptionHtml: "Quantum Physicist"
+  }
+];
