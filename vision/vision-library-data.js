5293086ffb1ca6a7545d646f5995da5d568f6ba7
Bob Tianqi Wei <bobwei3598@gmail.com>
2026-03-24 19:09:05 -0700

    update

---
+++ vision/vision-library-data.js
@@ -0,0 +1,37 @@
+// vision/vision-library-data.js developed by Bob Tianqi Wei
+window.VISION_LIBRARY_DATA = {
+  furtherReading: [
+    {
+      title: "Searching for the Non-Consequential: Dialectical Activities in HCI and the Limits of Computers",
+      author: "Haoqi Zhang",
+      href: "https://dl.acm.org/doi/full/10.1145/3613904.3641945"
+    },
+    {
+      title: "The Ecological Approach to Design",
+      author: "Takeshi Goto, Masato Sasaki, and Naoto Fukasawa",
+      href: "https://naotofukasawa.com/publications/1768/"
+    },
+    {
+      title: "Design for Body-Consciousness",
+      author: "Lintao Tang",
+      href: "https://mp.weixin.qq.com/s/C6D3C7z-k-TYfQ25YO8xEg"
+    },
+    {
+      title: "Design for Affordance: Teaching Reform Practice in the “Social Science and Product Innovation” Course",
+      author: "Lintao Tang",
+      href: "https://mp.weixin.qq.com/s/ZIeWlML8cfQKgjjwYsNg-A"
+    },
+    {
+      title: "TeleAbsence: A Vision of Past and Afterlife Telepresence",
+      author: "Hiroshi Ishii et al.",
+      href: "https://dspace.mit.edu/bitstream/handle/1721.1/158451/TeleAbsence_pres_a_00441.pdf?sequence=5&isAllowed=y"
+    }
+  ],
+  reflections: [
+    {
+      title: "The External Brain We've All Adored",
+      author: "Bob Tianqi Wei",
+      href: "https://youtu.be/vs9UzEYcjiY"
+    }
+  ]
+};
