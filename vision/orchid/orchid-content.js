6835fd8cea05d2cf91209ce370bf613c6f3a9b71
Bob Tianqi Wei <bobwei3598@gmail.com>
2026-03-29 16:49:40 -0700

    Update orchid-content.js

--- vision/orchid/orchid-content.js
+++ vision/orchid/orchid-content.js
@@ -15,7 +15,7 @@ window.VISION_REFLECTION_CONTENT = {
     {
       paragraphs: [
         "Shengmen Luo was my best friend in my undergraduate years.",
-        "In 2019, I just joined the Tsinghua University Student Art Troupe, I wasn’t very into Bach. Then I met Shengmen. We always have late night snacks together in Taoli dining hall and sit on the playground talking for hours. He took me through the back entrance of Meng Minwei Concert Hall, using the flashlight on his phone, played the grand piano for me and explain Bach’s music."
+        "In 2019, I had just joined the Tsinghua University Student Art Troupe, I wasn’t very into Bach. Then I met Shengmen. We would often have late night snacks together in Taoli dining hall and sit on the playground talking for hours. He took me through the back entrance of Meng Minwei Concert Hall, used the flashlight on his phone, played the grand piano for me and explain Bach’s music."
       ],
       audio: {
         src: "/vision/orchid/Goldberg_var13_Shengmen_Luo.mp3",
@@ -26,11 +26,11 @@ window.VISION_REFLECTION_CONTENT = {
     {
       paragraphs: [
         "Before dawn of April 8 2020, during the COVID-19 pandemic, he recorded the 13th variation of Bach’s Goldberg Variations on his old piano at home in Hunan. He didn't trim the hesitation and repetition at the beginning of the recording. He was sharing his interpretation and his emotions at that moment. It took me back to the concert hall at night. Then Goldberg Variations became my favorite piece. Among all its intertwining parts, Bach’s music can form limitless beauty no matter how the parts are combined or balanced.",
-        "That summer, I traveled to Changsha to visit him. He took me to Changjun High School, and we challenge ourselves with the spiciest Luosifen noodle, he tapped the first phrase of Stravinsky’s Petrushka on the table with chopsticks, and I tapped out the second phrase. Later when we met again in Beijing, he told me that from that moment, he considered me his zhiyin, his soulmate in music.",
+        "That summer, I traveled to Changsha to visit him. He took me to Changjun High School, and we challenge ourselves with the spiciest Luosifen noodle, he tapped the first phrase of Stravinsky’s Petrushka on the table with chopsticks, and I tapped out the second phrase. Later when we met again in Beijing, he told me that from that moment, he considered me his zhiyin, a soulmate in music.",
         "In 2023 when I graduated, Shengmen and I held a small concert together at the Tsinghua University Music Library. We opened the concert with a four hand of Bach’s BWV 106 (God’s time is the very best time). Later, at the graduation concert of the Student Art Troupe Clavier Team, we played an encore on two pianos at New Tsinghua School Concert Hall, my own arrangement of Zhao Jiping’s Lieder, Virtues of Orchid (《幽蘭操》). I kept the final lyrics:",
         "“蘭之猗猗，揚揚其香。眾香拱之，幽幽其芳。” (“The orchids flourish, their fragrance drifting high. Surrounded by other flowers, yet their scent remains pure and deep.”)",
         "as a farewell to my undergraduate years.",
-        "Shengmen was reported missing in Feb 2025 in Netherlands, later found suicided in a lake near his home. His Goldberg Variation 13 is still my bedtime alarm. It always take me back to the concert hall under the glow of a phone flashlight."
+        "Shengmen was reported missing in Feb 2025 in Netherlands, later found suicided in a lake near his home. His Goldberg Variation 13 is still my bedtime alarm. It always takes me back to the concert hall under the glow of a phone flashlight."
       ],
       video: {
         src: "https://www.youtube.com/embed/qbW99RrqNqw?si=VH5fy08xDDw6F0YP",
