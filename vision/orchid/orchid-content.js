91b37fc91928868b8e4514d40927cbc25e61b308
Bob Tianqi Wei <bobwei3598@gmail.com>
2026-03-30 00:05:30 -0700

    Update orchid-content.js

--- vision/orchid/orchid-content.js
+++ vision/orchid/orchid-content.js
@@ -15,7 +15,7 @@ window.VISION_REFLECTION_CONTENT = {
     {
       paragraphs: [
         "Shengmen Luo was my best friend in my undergraduate years.",
-        "In 2019, I had just joined the Tsinghua University Student Art Troupe, and at that time I wasn’t very into Bach. Then I met Shengmen. We would often have late night snacks together in Taoli dining hall and sit on the playground talking for hours. He took me through the back entrance of Meng Minwei Concert Hall, used the flashlight on his phone, played the grand piano for me and explained Bach’s music."
+        "In 2019, I had just joined the Tsinghua University Student Art Troupe, and at that time I wasn’t very into Bach. Then I met Shengmen. We would often have late night snacks together in Taoli dining hall and sit on the playground talking for hours. He took me through the back entrance of Meng Minwei Concert Hall, and, using the flashlight on his phone, played the grand piano for me while explaining Bach’s music."
       ],
       audio: {
         src: "/vision/orchid/Goldberg_var13_Shengmen_Luo.mp3",
@@ -25,7 +25,7 @@ window.VISION_REFLECTION_CONTENT = {
     },
     {
       paragraphs: [
-        "Before dawn on April 8,2020, during the COVID-19 pandemic, he recorded Variation 13 of Bach’s Goldberg Variations on his old piano at home in Hunan. He didn't trim the hesitation and repetition at the beginning of the recording. He was sharing his interpretation and his emotions at that moment. It took me back to the concert hall at night. Then the Goldberg Variations became my favorite piece.",
+        "Before dawn on April 8, 2020, well into the COVID-19 pandemic, he recorded Variation 13 of Bach’s Goldberg Variations on an old piano at his home in Hunan. He didn't trim his hesitation and repetition at the beginning of the recording. He was sharing his interpretation and his emotions at that moment. It took me back to the concert hall at night. Then the Goldberg Variations became my favorite piece.",
         "That summer, I traveled to Changsha to visit him. He took me to Changjun High School, and we challenged ourselves with the spiciest Luosifen noodles. He tapped the first phrase of Stravinsky’s Petrushka on the table with chopsticks, and I tapped out the second phrase. Later when we met again in Beijing, he told me that from that moment, he considered me his zhiyin.",
         "In 2023, when I graduated, Shengmen and I held a small concert together at the Tsinghua University Music Library. We opened the concert with a four hand performance of Bach’s BWV 106 (God’s Time is the Very Best Time). Later, at the graduation concert of the Student Art Troupe Clavier Team, we played an encore on two pianos in New Tsinghua School Concert Hall: my own arrangement of Zhao Jiping’s Lieder, Virtues of the Silent Orchid (《幽蘭操》). I kept the final lyrics:",
         "“蘭之猗猗，揚揚其香。眾香拱之，幽幽其芳。” (“The orchids flourish, their fragrance drifting high. Surrounded by other flowers, yet their scent remains pure and deep.”)",
