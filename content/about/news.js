2b3f605063547903b01868a890ba1bb36ddd531b
Bob Tianqi Wei <bobwei3598@gmail.com>
2026-06-25 11:44:49 -0700

    Refine about news items and toggle icon

--- content/about/news.js
+++ content/about/news.js
@@ -1,13 +1,43 @@
 // news.js developed by Bob Tianqi Wei
 window.aboutNewsItems = [
-  "I am so excited to be joining the MIT Media Lab, <a href=\"https://www.media.mit.edu/groups/tangible-media/overview/\" target=\"_blank\" class=\"link-in-paragraph\">Tangible Media Group</a> in fall 2026!",
-  "Our paper <a href=\"https://doi.org/10.1145/3746059.3747685\" target=\"_blank\" class=\"link-in-paragraph\"><em>MorphingSkin</em></a> received the Best Paper Award at ACM UIST 2025!",
-  "Our LBW paper <a href=\"https://doi.org/10.1145/3706599.3719807\" target=\"_blank\" class=\"link-in-paragraph\"><em>EKPHRASIS</em></a> was accepted to ACM CHI 2025!",
-  "Our paper <a href=\"https://doi.org/10.1145/3715275.3732082\" target=\"_blank\" class=\"link-in-paragraph\"><em>Labor, Power, and Belonging: The Work of Voice in the Age of AI Reproduction</em></a> was accepted to ACM FAccT 2025!",
-  "I had the honor of delivering a speech at the UC Berkeley Master of Design graduation ceremony at Hertz Hall on December 22, 2024.",
-  "Shayne Shen and I presented <em>EKPHRASIS</em> at the 2024 UC Berkeley MDes Graduate Exhibition “VEINS OF THE COSMOS” on December 20, 2024.",
-  "My research group and I presented our paper <em>Romantic Breakups as a Lens for Industrial Cybersecurity: Cross-Domain Insights for Access Control</em> at the 2024 Jacobs Winter Design Showcase: HCI Research on December 17, 2024.",
-  "My research group and I presented the <a href=\"https://dl.acm.org/doi/10.1145/3672539.3686783\" target=\"_blank\" class=\"link-in-paragraph\">Demonstration of Symphony Orchestra</a> at <a href=\"https://uist.acm.org/2024/\" target=\"_blank\" class=\"link-in-paragraph\">UIST 2024</a>.",
-  "<a href=\"/images/carillon-poster.jpg\" target=\"_blank\" class=\"link-in-paragraph\">Abigail &amp; Bob's Carillon Recital took place on April 28, 2024 at 6pm in UC Berkeley Sather Tower</a>.",
-  "I presented my research project <a href=\"https://drive.google.com/file/d/1fcP8EizJDB_8-QX0nd0WQMWcR37ULKwZ/view\" target=\"_blank\" class=\"link-in-paragraph\"><em>Building Professional Hearing</em></a> on April 5, 2024 at the <a href=\"https://www.ocf.berkeley.edu/~rdoc/\" target=\"_blank\" class=\"link-in-paragraph\">UC Berkeley Education Research Day Conference</a>."
+  {
+    date: "Fall 2026",
+    textHtml: "I am so excited to be joining the MIT Media Lab, <a href=\"https://www.media.mit.edu/groups/tangible-media/overview/\" target=\"_blank\" class=\"link-in-paragraph\">Tangible Media Group</a>!"
+  },
+  {
+    date: "UIST 2025",
+    textHtml: "Our paper <a href=\"https://doi.org/10.1145/3746059.3747685\" target=\"_blank\" class=\"link-in-paragraph\"><em>MorphingSkin</em></a> received the Best Paper Award!"
+  },
+  {
+    date: "CHI 2025",
+    textHtml: "Our LBW paper <a href=\"https://doi.org/10.1145/3706599.3719807\" target=\"_blank\" class=\"link-in-paragraph\"><em>EKPHRASIS</em></a> was accepted!"
+  },
+  {
+    date: "FAccT 2025",
+    textHtml: "Our paper <a href=\"https://doi.org/10.1145/3715275.3732082\" target=\"_blank\" class=\"link-in-paragraph\"><em>Labor, Power, and Belonging: The Work of Voice in the Age of AI Reproduction</em></a> was accepted!"
+  },
+  {
+    date: "Dec 22, 2024",
+    textHtml: "I had the honor of delivering a speech at the UC Berkeley Master of Design graduation ceremony at Hertz Hall."
+  },
+  {
+    date: "Dec 20, 2024",
+    textHtml: "Shayne Shen and I presented <em>EKPHRASIS</em> at the 2024 UC Berkeley MDes Graduate Exhibition “VEINS OF THE COSMOS”."
+  },
+  {
+    date: "Dec 17, 2024",
+    textHtml: "My research group and I presented our paper <em>Romantic Breakups as a Lens for Industrial Cybersecurity</em> at the 2024 Jacobs Winter Design Showcase: HCI Research."
+  },
+  {
+    date: "UIST 2024",
+    textHtml: "My research group and I presented the <a href=\"https://dl.acm.org/doi/10.1145/3672539.3686783\" target=\"_blank\" class=\"link-in-paragraph\">Demonstration of Symphony Orchestra</a>."
+  },
+  {
+    date: "Apr 28, 2024",
+    textHtml: "<a href=\"/images/carillon-poster.jpg\" target=\"_blank\" class=\"link-in-paragraph\">Abigail &amp; Bob's Carillon Recital took place at 6pm in UC Berkeley Sather Tower</a>."
+  },
+  {
+    date: "Apr 5, 2024",
+    textHtml: "I presented my research project <a href=\"https://drive.google.com/file/d/1fcP8EizJDB_8-QX0nd0WQMWcR37ULKwZ/view\" target=\"_blank\" class=\"link-in-paragraph\"><em>Building Professional Hearing</em></a> at the <a href=\"https://www.ocf.berkeley.edu/~rdoc/\" target=\"_blank\" class=\"link-in-paragraph\">UC Berkeley Education Research Day Conference</a>."
+  }
 ];
