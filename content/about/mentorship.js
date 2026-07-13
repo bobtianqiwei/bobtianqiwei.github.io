09af13a6aebba3bff3f25d4edc66133713d62e9b
Bob Tianqi Wei <bobwei3598@gmail.com>
2026-07-13 12:05:29 -0700

    Update mentorship.js

--- content/about/mentorship.js
+++ content/about/mentorship.js
@@ -3,6 +3,6 @@ window.aboutMentorship = [
   {
     href: "https://www.andyshensiyang.com/",
     name: "Andy Siyang Shen",
-    descriptionHtml: "Robotics Researcher<br>2024-2025"
+    descriptionHtml: "Robotics Researcher<br>2023-2024"
   }
 ];
